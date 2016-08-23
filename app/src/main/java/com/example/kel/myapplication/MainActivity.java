77d4d4c47d103f2ff9d9caf19dff9cf5cd8a4bd5
kelvinkipsang <kelvinkipsang@gmail.com>
2016-08-23 16:18:52 +0300

    adding intent

--- app/src/main/java/com/example/kel/myapplication/MainActivity.java
+++ app/src/main/java/com/example/kel/myapplication/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.kel.myapplication;
 
+import android.content.Intent;
+import android.support.v4.app.ShareCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Thread bg = new Thread(){
+            public void run(){
+                try{
+                    sleep(2000);
+                    Intent intent = new Intent(MainActivity.this,LoginActivity.class);
+                    StartActivity(intent);
+                    finish();
+                }
+                catch(Exception e){
+                    e.printStackTrace();
+                }
+            }
+        };
     }
 }
