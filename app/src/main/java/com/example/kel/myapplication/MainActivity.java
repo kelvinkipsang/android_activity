465ebd9c95f9f65dd75ab9680b46e04b4874db64
kelvinkipsang <kelvinkipsang@gmail.com>
2016-08-23 17:12:47 +0300

    adding start fnt

--- app/src/main/java/com/example/kel/myapplication/MainActivity.java
+++ app/src/main/java/com/example/kel/myapplication/MainActivity.java
@@ -1,7 +1,7 @@
 package com.example.kel.myapplication;
 
+
 import android.content.Intent;
-import android.support.v4.app.ShareCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -11,18 +11,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
         Thread bg = new Thread(){
-            public void run(){
-                try{
+          public void run(){
+              try{
                     sleep(2000);
-                    Intent intent = new Intent(MainActivity.this,LoginActivity.class);
-                    StartActivity(intent);
-                    finish();
-                }
-                catch(Exception e){
-                    e.printStackTrace();
-                }
-            }
+                  Intent intent =new Intent(MainActivity.this, LoginActivity.class);
+                  startActivity(intent);
+                  finish();
+              }catch (Exception e){
+                  e.printStackTrace();
+              }
+          }
         };
+        bg.start();
     }
 }
+
